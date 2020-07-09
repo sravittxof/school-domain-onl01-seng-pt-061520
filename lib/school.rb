2698538b6183bedf6dd828259d2692b1c7e9a1a9
Travis Fox <travis.fox90@gmail.com>
2020-07-09 01:15:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.keys.include?(grade) == false
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end  
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  
+end
